0cae77292f5c5c8a9c46378776f18d9ef2622537
Prageeth <prag123@gmail.com>
2019-11-07 19:59:40 -0500

    Initial version

---
+++ app/src/main/java/com/prageeth/square/oppo/model/Employee.kt
@@ -0,0 +1,5 @@
+package com.prageeth.square.oppo.model
+
+/**
+ * Created  by Prageeth on 2019-11-07
+ */
---
+++ app/src/main/java/com/prageeth/square/oppo/repo/EmployeeRepository.kt
@@ -0,0 +1,7 @@
+package com.prageeth.square.oppo.repo
+
+/**
+ * Created  by Prageeth on 2019-11-07
+ */
+class EmployeeRepository {
+}
